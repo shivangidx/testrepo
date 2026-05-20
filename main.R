bcb6b40e923eb4e436bc1bfdf01dc7ab86ee5d09
dxshivangi <shivangidixit2808@gmail.com>
2026-05-21 01:04:15 +0530

    new commit

---
+++ main.R
@@ -0,0 +1,5 @@
+library(tidyverse)
+library(here)
+tv_hours_table <- gss_cat %>% group_by(marital) %>% summarise(mean_tv_hours = mean(tvhours, na.rm=TRUE))
+
+write_csv(tv_hours_table,here("tv_hours_by_marital.csv"))
