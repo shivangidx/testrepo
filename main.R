cf352391b1e2921d26cd90531c9453fdc21418c4
dxshivangi <shivangidixit2808@gmail.com>
2026-05-23 17:35:50 +0530

    long name

--- main.R
+++ main.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 library(here)
-tv_hours_table <- gss_cat %>% filter(age < 30) %>%
+tv_hours_tabular <- gss_cat %>% filter(age < 30) %>%
   group_by(marital) %>% summarise(mean_tv_hours = mean(tvhours, na.rm=TRUE))
 
-write_csv(tv_hours_table,here("tv_hours_by_marital.csv"))
+write_csv(tv_hours_tabular,here("tv_hours_by_marital.csv"))
